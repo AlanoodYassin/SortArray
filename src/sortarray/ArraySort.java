1708d08b56dd33dd924f981ac0ac9208af83e944
AlanoodYassin <alanood.yassin@hotmail.com>
2020-08-22 20:51:32 +0400

    the first branch of extra algorithms 1quick sort

--- src/sortarray/ArraySort.java
+++ src/sortarray/ArraySort.java
@@ -28,18 +28,14 @@ public class ArraySort {
        void selectionsort(int arr[]) 
     { 
         int n = arr.length; 
-  
-        // One by one move boundary of unsorted subarray 
         for (int i = 0; i < n-1; i++) 
         { 
-            // Find the minimum element in unsorted array 
+       
             int min_idx = i; 
             for (int j = i+1; j < n; j++) 
                 if (arr[j] < arr[min_idx]) 
                     min_idx = j; 
   
-            // Swap the found minimum element with the first 
-            // element 
             int temp = arr[min_idx]; 
             arr[min_idx] = arr[i]; 
             arr[i] = temp; 
@@ -55,6 +51,41 @@ public class ArraySort {
             System.out.print(arr[i] + " "); 
         System.out.println(); 
     } 
+          
+           int partition(int arr[], int low, int high) 
+    { 
+        int pivot = arr[high];  
+        int i = (low-1); 
+        for (int j=low; j<high; j++) 
+        { 
+            if (arr[j] < pivot) 
+            { 
+                i++; 
+                int temp = arr[i]; 
+                arr[i] = arr[j]; 
+                arr[j] = temp; 
+            } 
+        } 
+        int temp = arr[i+1]; 
+        arr[i+1] = arr[high]; 
+        arr[high] = temp; 
+  
+        return i+1; 
+    } 
+  
+  
+
+    void sort(int arr[], int low, int high) 
+    { 
+        if (low < high) 
+        { 
+            int pi = partition(arr, low, high); 
+            sort(arr, low, pi-1); 
+            sort(arr, pi+1, high); 
+        } 
+    } 
+          
+          
   
     // Driver method to test above 
     public static void main(String args[]) 
